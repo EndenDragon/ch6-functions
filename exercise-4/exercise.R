ea66aa16fbae143f0c28442eb2560e52ee43ff96
Jeremy "EndenDragon" Zhang <jeremyzhang5000@gmail.com>
2018-04-04 14:14:08 -0700

    Complete exercise 4

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -4,12 +4,15 @@
 # returns whether or not (e.g., a boolean) the length of one argument is greater
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
-
+is_twice_as_long <- function (str1, str2) {
+  return(nchar(str1) >= nchar(str2))
+}
 
 # Call your `is_twice_as_long` function by passing it different length strings
 # to confirm that it works. Make sure to check when _either_ argument is twice
 # as long, as well as when neither are!
-
+print(is_twice_as_long("dog", "a"))
+print(is_twice_as_long("a", "dog"))
 
 
 # Define a function `describe_difference` that takes in two strings. The
@@ -17,8 +20,18 @@
 #   "Your first string is longer by N characters"
 #   "Your second string is longer by N characters"
 #   "Your strings are the same length!"
+describe_difference <- function (s1, s2) {
+  if (nchar(s1) > nchar(s2)) {
+    return(paste("Your first string is longer by", nchar(s1) - nchar(s2), "characters"))
+  } else if (nchar(s1) < nchar(s2)) {
+    return(paste("Your second string is longer by", nchar(s2) - nchar(s1), "characters"))
+  }
+  return("Your strings are the same length!")
+}
 
 
 # Call your `describe_difference` function by passing it different length strings
 # to confirm that it works. Make sure to check all 3 conditions1
-
+print(describe_difference("Rainbow Dash", "Flutters"))
+print(describe_difference("Rainbow", "Fluttershy"))
+print(describe_difference("Rainbow", "Rainbow"))
